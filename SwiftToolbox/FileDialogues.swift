d222f4fa830bb1a3cd0c14bf4501b8dbddeed133
zap.zip <zapdotzip@gmail.com>
2025-06-22 15:02:15 -0700

    clean up openPanel, add openPanelModal

--- SwiftToolbox/FileDialogues.swift
+++ SwiftToolbox/FileDialogues.swift
@@ -6,21 +6,67 @@
 import AppKit
 
 public class FileDialogues {
-	/// Creates an Open/Save Dialogue panel for the user.
-	/// - Parameters:
-	/// - Returns: The panel itself, to get URLs from, and the response from the user.
-	public static func openPanel(message: String, prompt: String, canChooseDirectories: Bool, canChooseFiles: Bool, allowsMultipleSelection: Bool, canCreateDirectories: Bool, allowedFileTypes: [String]? = nil) -> (NSOpenPanel, NSApplication.ModalResponse) {
+	public typealias completionHandler = ([URL]?) -> Void
+	
+	private static func createOpenPanel(message: String?, prompt: String?, canChooseDirectories: Bool, canChooseFiles: Bool, canSelectMultipleItems: Bool, canCreateDirectories: Bool, allowedFileTypes: [String]? = nil) -> NSOpenPanel {
 		let openPanel = NSOpenPanel()
-		openPanel.message = message
-		openPanel.prompt = prompt
+		if let message {
+			openPanel.message = message
+		}
+		if let prompt {
+			openPanel.prompt = prompt
+		}
 		openPanel.canChooseDirectories = canChooseDirectories
 		openPanel.canChooseFiles = canChooseFiles
-		openPanel.allowsMultipleSelection = allowsMultipleSelection
+		openPanel.allowsMultipleSelection = canSelectMultipleItems
 		openPanel.canCreateDirectories = canCreateDirectories
-		if allowedFileTypes != nil {
+		if let allowedFileTypes {
 			openPanel.allowedFileTypes = allowedFileTypes
 		}
-		return (openPanel, openPanel.runModal())
+		return openPanel
+	}
+	
+	/// Creates an Open/Save Dialogue panel for the user.
+	/// - Parameters:
+	///   - message: The message to display to the user, nothing if unspecified.
+	///   - prompt: The select button text, "Open" if unspecified.
+	///   - canChooseDirectories: If the user can select directories.
+	///   - canChooseFiles: If the user can choose files.
+	///   - canSelectMultipleItems: If the user can choose multiple items.
+	///   - canCreateDirectories: If the user can create directories.
+	///   - allowedFileTypes: The files types that are allowed to be selected. All items are allowed if unspecified.
+	/// - Returns: An array of selected `URL`s, or `nil` if the user clicked the cancel button.
+	public static func openPanel(message: String?, prompt: String?, canChooseDirectories: Bool, canChooseFiles: Bool, canSelectMultipleItems: Bool, canCreateDirectories: Bool, allowedFileTypes: [String]? = nil) -> [URL]? {
+		let openPanel = createOpenPanel(message: message, prompt: prompt, canChooseDirectories: canChooseDirectories, canChooseFiles: canChooseFiles, canSelectMultipleItems: canSelectMultipleItems, canCreateDirectories: canCreateDirectories, allowedFileTypes: allowedFileTypes)
+		
+		if openPanel.runModal() == .OK {
+			return openPanel.urls
+		} else {
+			return nil
+		}
+	}
+	
+	/// Creates an Open/Save Dialogue panel for the user.
+	/// - Parameters:
+	///   - message: The message to display to the user, nothing if unspecified.
+	///   - prompt: The select button text, "Open" if unspecified.
+	///   - canChooseDirectories: If the user can select directories.
+	///   - canChooseFiles: If the user can choose files.
+	///   - canSelectMultipleItems: If the user can choose multiple items.
+	///   - canCreateDirectories: If the user can create directories.
+	///   - allowedFileTypes: The files types that are allowed to be selected. All items are allowed if unspecified.
+	///   - handler: Receives an array of selected `URL`s, or `nil` if the user clicked the cancel button..
+	public static func openPanelModal(for window: NSWindow, message: String?, prompt: String?, canChooseDirectories: Bool, canChooseFiles: Bool, canSelectMultipleItems: Bool, canCreateDirectories: Bool, allowedFileTypes: [String]? = nil, handler: @escaping completionHandler) {
+		let openPanel = createOpenPanel(message: message, prompt: prompt, canChooseDirectories: canChooseDirectories, canChooseFiles: canChooseFiles, canSelectMultipleItems: canSelectMultipleItems, canCreateDirectories: canCreateDirectories, allowedFileTypes: allowedFileTypes)
+		
+		openPanel.beginSheetModal(for: window) { response in
+			openPanel.orderOut(nil)
+			if response == .OK {
+				handler(openPanel.urls)
+			} else {
+				handler(nil)
+			}
+		}
 	}
 	
 	public static func savePanel(title: String, message: String, nameFieldLabel: String, nameField: String, currentDirectory: URL?, canCreateDirectories: Bool, canSelectHiddenExtension: Bool) -> (URL?, NSApplication.ModalResponse) {
