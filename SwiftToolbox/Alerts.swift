ca2dd237fa44fc48c8654df3339ca9690c478bad
zap.zip <zapdotzip@gmail.com>
2025-07-11 10:46:29 -0700

    add PopoverTextAlert

--- SwiftToolbox/Alerts.swift
+++ SwiftToolbox/Alerts.swift
@@ -47,4 +47,35 @@ public class Alerts {
 		return alert.runModal() == .alertFirstButtonReturn
 	}
 	
+	
+	/// Creates an `NSPopover` with a single label relative to a view.
+	/// - Parameters:
+	///   - text: The text to display in the label
+	///   - view: The view to display the popover from.
+	///   - preferredEdge: The edge of positioningView the popover should prefer to be anchored to.
+	/// - Returns: The NSPopover, for reuse.
+	@available(macOS 10.12, *)
+	public static func PopoverTextAlert(text: String, relativeTo view: NSView, preferredEdge: NSRectEdge) -> NSPopover {
+		let label = NSTextField(wrappingLabelWithString: text)
+		label.alignment = .left
+		label.sizeToFit()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		let contentViewController = NSViewController()
+		let viewFrame = NSRect(x: 0, y: 0, width: label.frame.width + 16, height: label.frame.height + 16)
+		contentViewController.view = NSView(frame: viewFrame)
+		contentViewController.view.wantsLayer = true
+		contentViewController.view.addSubview(label)
+		let popover = NSPopover()
+		popover.contentViewController = contentViewController
+		popover.behavior = .semitransient
+		NSLayoutConstraint.activate([
+			label.topAnchor.constraint(equalTo: contentViewController.view.topAnchor, constant: 8),
+			label.trailingAnchor.constraint(equalTo: contentViewController.view.trailingAnchor, constant: -8),
+			label.bottomAnchor.constraint(equalTo: contentViewController.view.bottomAnchor, constant: -8),
+			label.leadingAnchor.constraint(equalTo: contentViewController.view.leadingAnchor, constant: 8)
+		])
+		popover.show(relativeTo: view.bounds, of: view, preferredEdge: preferredEdge)
+		return popover
+	}
+	
 }
